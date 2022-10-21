80a2a9d8a54d21449a3414ac6b7a1b11ef39df57
Tilan Prabudda <70626483+tilanprabudda@users.noreply.github.com>
2022-10-21 21:37:57 +0530

    Update register.php

--- account/register.php
+++ account/register.php
@@ -29,117 +29,118 @@ else{
 
   <link href="js/plugins/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen,projection">
 
-    <style type="text/css">
-  .input-field div.error{
-    position: relative;
-    top: -1rem;
-    left: 0rem;
-    font-size: 0.8rem;
-    color:#FF4081;
-    -webkit-transform: translateY(0%);
-    -ms-transform: translateY(0%);
-    -o-transform: translateY(0%);
-    transform: translateY(0%);
-  }
-  .input-field label.active{
-      width:100%;
-  }
-  .left-alert input[type=text] + label:after, 
-  .left-alert input[type=password] + label:after, 
-  .left-alert input[type=email] + label:after, 
-  .left-alert input[type=url] + label:after, 
-  .left-alert input[type=time] + label:after,
-  .left-alert input[type=date] + label:after, 
-  .left-alert input[type=datetime-local] + label:after, 
-  .left-alert input[type=tel] + label:after, 
-  .left-alert input[type=number] + label:after, 
-  .left-alert input[type=search] + label:after, 
-  .left-alert textarea.materialize-textarea + label:after{
-      left:0px;
-  }
-  .right-alert input[type=text] + label:after, 
-  .right-alert input[type=password] + label:after, 
-  .right-alert input[type=email] + label:after, 
-  .right-alert input[type=url] + label:after, 
-  .right-alert input[type=time] + label:after,
-  .right-alert input[type=date] + label:after, 
-  .right-alert input[type=datetime-local] + label:after, 
-  .right-alert input[type=tel] + label:after, 
-  .right-alert input[type=number] + label:after, 
-  .right-alert input[type=search] + label:after, 
-  .right-alert textarea.materialize-textarea + label:after{
-      right:70px;
-  }
-  </style> 
 
-<style>
-    html,body{
-background-image: url('http://getwallpapers.com/wallpaper/full/a/5/d/544750.jpg');
-background-size: cover;
-height: 100%;
-font-family: 'Numans', sans-serif;
+  <style>
+.gradient-custom-2 {
+/* fallback for old browsers */
+background: #fccb90;
+
+/* Chrome 10-25, Safari 5.1-6 */
+background: -webkit-linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+
+/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+background: linear-gradient(to right, #247d04, #3ac812, #41ac10, #226f06);
 }
-</style>
-</head>
 
+@media (min-width: 375px) {
+.gradient-form {
+width: auto !important;
+}
+}
+@media (min-width: 375px) {
+.gradient-custom-2 {
+border-top-right-radius: .2rem;
+border-bottom-right-radius: .2rem;
+}
+}
+@media (min-width: 768px) {
+.gradient-form {
+height: 100vh !important;
+}
+}
+@media (min-width: 769px) {
+.gradient-custom-2 {
+border-top-right-radius: .3rem;
+border-bottom-right-radius: .3rem;
+}
+}
+
+  </style>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="styles.css">
 
   <!-- End Page Loading -->
 
+<section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-10">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0">
+            <div class="col-lg-6">
+              <div class="card-body p-md-5 mx-md-4">
 
+                <div class="text-center">
+                  <img src="images/logon.png"
+                    style="width: 185px;" alt="logo">
+                  <h4 class="mt-1 mb-5 pb-1">We are The Shop Ceylon Team</h4>
+                </div>
 
-  <div id="login-page" class="row">
-    <div class="col s12 z-depth-4 card-panel">
-      <form class="formValidate" id="formValidate" method="post" action="routers/register-router.php" novalidate="novalidate" class="col s12">
-        <div class="row">
-          <div class="input-field col s12 center">
-            <h4>Register for Shop Ceylon System</h4>
-          </div>
-        </div>
-        <div class="row margin">
-          <div class="input-field col s12">
-            <i class="mdi-social-person-outline prefix"></i>
-            <input name="username" id="username" type="text"  data-error=".errorTxt1">
-            <label for="username" class="center-align">Username</label>
-			<div class="errorTxt1"></div>			
-          </div>
-        </div>
-        <div class="row margin">
-          <div class="input-field col s12">
-            <i class="mdi-social-person prefix"></i>
-            <input name="name" id="name" type="text" data-error=".errorTxt2">
+                
+                <form class="formValidate" id="formValidate" method="post" action="routers/register-router.php" novalidate="novalidate">
+                  <p>Register for Shop Ceylon System</p>
+
+                  <input name="username" id="username" type="text"  data-error=".errorTxt1">
+                   <label for="username" class="center-align">Username</label>
+
+                   <input name="name" id="name" type="text" data-error=".errorTxt2">
             <label for="name" class="center-align">Name</label>
-			<div class="errorTxt2"></div>			
-          </div>
-        </div>
-        <div class="row margin">
-          <div class="input-field col s12">
-            <i class="mdi-action-lock-outline prefix"></i>
+
             <input name="password" id="password" type="password" data-error=".errorTxt3">
             <label for="password">Password</label>
-			<div class="errorTxt3"></div>			
-          </div>
-        </div>
-        <div class="row margin">
-          <div class="input-field col s12">
-            <i class="mdi-communication-phone prefix"></i>
+
+
             <input name="phone" id="phone" type="number" data-error=".errorTxt4">
             <label for="phone">Phone</label>
-			<div class="errorTxt4"></div>			
-          </div>
-        </div>		
-        <div class="row">
-          <div class="input-field col s12">
-			<a href="javascript:void(0);" onclick="document.getElementById('formValidate').submit();" class="waves-effect waves-light btn-large">Register</a>
-          </div>
-          <div class="input-field col s12">
-            <p class="margin center medium-small sign-up">Already have an account? <a href="login.php">Login</a></p>
+
+
+                  <div class="text-center pt-1 mb-5 pb-1">
+                  <a href="javascript:void(0);" onclick="document.getElementById('formValidate').submit();" button class="btn btn-success btn-block fa-lg btn btn-success" type="button">Register
+                      </button></a>
+                    
+                  </div>
+
+                  <div class="d-flex align-items-center justify-content-center pb-4">
+                    <p class="mb-0 me-2">Already have an account?</p>
+                    <a href="login.php" button type="button" class="btn btn btn-success">Log in</button></a>
+                  </div>
+
+                </form>
+
+              </div>
+            </div>
+            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+              <img src="images/logon.png"
+                    style="width: 400px;" alt="logo">
+                <h4 class="mb-4">We are a Web Application for E-commerce</h4>
+                <p class="small mb-0">Our aim is to sell local handicrafts and products internationally and across Sri Lanka through. We will try to improve customer-friendly service</p>
+              </div>
+            </div>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   </div>
+</section>
 
-
+</div>
 
   <!-- ================================================
     Scripts
